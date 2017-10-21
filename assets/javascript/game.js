3e4771c6a6aae2d9df25c25f577b15414f1930c2
wesdriver45 <wesdriver45@gmail.com>
2017-10-20 22:51:31 -0400

    cssandjschanges

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,17 +16,69 @@ $(document).ready(function() {
 	random = Math.floor((Math.random() * 119) +1);
 	$("#random").text(random);
 
-	gemOne = Math.floor((Math.random() *11) + 1)
-	gemTwo = Math.floor((Math.random() *11) + 1)
-	gemThree = Math.floor((Math.random() *11) + 1)
-	gemFour = Math.floor((Math.random() *11) + 1)
+	//determines a value for each gem b/t 1-12
+	gemOne = Math.floor((Math.random() *11) + 1);
+	gemTwo = Math.floor((Math.random() *11) + 1);
+	gemThree = Math.floor((Math.random() *11) + 1);
+	gemFour = Math.floor((Math.random() *11) + 1);
+
+	$("#wins").text(wins);
+	$("#losses").text(losses);
+	
+	//click functions for gems
+	$("#gemOne").on("click", function() {
+		gemTotal = gemTotal + gemOne;
+			$("#gemTotal").text(gemTotal);
+
+			if (gemTotal === random) {
+				wins ++;
+			} else if (gemTotal > random) {
+				losses++;
+			}
+	});
+
+	$("#gemTwo").on("click", function() {
+		gemTotal = gemTotal + gemTwo;
+			$("#gemTotal").text(gemTotal);
+
+			if (gemTotal === random) {
+				wins ++;
+			} else if (gemTotal > random) {
+				losses++;
+			}
+	});
+
+	$("#gemThree").on("click", function() {
+		gemTotal = gemTotal + gemThree;
+			$("#gemTotal").text(gemTotal);
+
+			if (gemTotal === random) {
+				wins ++;
+			} else if (gemTotal > random) {
+				losses++;
+			}
+	});
+
+	$("#gemFour").on("click", function() {
+		gemTotal = gemTotal + gemFour;
+			$("#gemTotal").text(gemTotal);
+
+			if (gemTotal === random) {
+				wins ++;
+			} else if (gemTotal > random) {
+				losses++;
+			}
+	});
+
+
+
 
 
 	});
 
 
 //clicking a gem starts the game
-//also determines a value for each gem b/t 1-12
+
 //each click updates the player's score count
 //tally wins and losses
 //reset gems, random number and gem total
